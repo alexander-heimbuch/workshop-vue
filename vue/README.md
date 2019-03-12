0e902d66a6761656911935d34d42f063478bd203
Alexander Heimbuch <github@heimbu.ch>
2019-03-12 07:45:57 +0100

    init: vue

---
+++ vue/README.md
@@ -0,0 +1,25 @@
+# Vue Movies
+
+## Install CLI
+
+```bash
+npm install -g @vue/cli
+```
+
+## Bootstrap Workspace
+
+```bash
+vue create vue
+```
+
+## Install Dependencies
+
+``bash
+npm i sass-loader node-sass --save-dev
+```
+
+## Start Development
+
+```bash
+npm run serve
+```
---
+++ vue/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    background: #31394c;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-size: 14px;
+    line-height: 1.5;
+    color: #24292e;
+  }
+
+  button {
+    border: none;
+    margin: 0;
+    padding: 0;
+    width: auto;
+    overflow: visible;
+    background: transparent;
+    /* inherit font & color from ancestor */
+    color: inherit;
+    font: inherit;
+    /* Normalize `line-height`. Cannot be changed from `normal` in Firefox 4+. */
+    line-height: normal;
+    /* Corrects font smoothing for webkit */
+    -webkit-font-smoothing: inherit;
+    -moz-osx-font-smoothing: inherit;
+    /* Corrects inability to style clickable `input` types in iOS */
+    -webkit-appearance: none;
+    /* Remove excess padding and border in Firefox 4+ */
+  }
+
+  button::-moz-focus-inner {
+    border: 0;
+    padding: 0;
+  }
+
+  input {
+    border: none;
+    background-image: none;
+    background-color: transparent;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+  }
+</style>
